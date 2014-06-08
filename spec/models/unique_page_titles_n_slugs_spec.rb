bd0a963257df2ec31cccc8497dd9588a88f9dd99
Mark Van <markfeedly@gmail.com>
2014-06-08 21:58:20 +0100

    refactored unique page spec

--- spec/models/unique_page_titles_n_slugs_spec.rb
+++ spec/models/unique_page_titles_n_slugs_spec.rb
@@ -4,10 +4,10 @@ describe 'Pages with unique titles' do
 
   let(:user) {FactoryGirl.create(:user) }
   let(:page){ FactoryGirl.create(:page, title: 'first title', user: user, content: 'any' ) }
-  let(:ps){ page.history.last }
 
   it 'should not save a page with the same title' do
-    Page.create(title: page.title, user: user, content: 'any' ).id.should == nil
+    expect{Page.create!(title: page.title, user: user, content: 'any' )}.
+      to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Title has already been taken, Slug has already been taken')
   end
 
 end
