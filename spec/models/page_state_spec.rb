a97b924993349a2a8446acc85e1bdcfbb80bb120
Mark Van <markfeedly@gmail.com>
2014-06-08 15:51:06 +0100

    did some work on page state spec still needs more work

--- spec/models/page_state_spec.rb
+++ spec/models/page_state_spec.rb
@@ -2,16 +2,8 @@ require 'spec_helper'
 
 describe 'Previous Page' do
 
-  def user() @user end
-  def page() @page end
-  def page_state() @page_state end
-
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-    @page = FactoryGirl.create(:page)
-    @page_state = FactoryGirl.create(:page_state, title: page.original_title, content: 'the content',
-                                     user: user, page: page, tags: 'tag1, tag2')
-  end
+  let(:page){ FactoryGirl.create(:page, title: 'Title', content: 'Content', tags: 't1, t2', categories: 'c1, c2') }
+  let(:page_state) { page.history.last }
 
   it { page_state.should validate_presence_of(:page_id) }
   it { page_state.should belong_to(:page) }
@@ -23,17 +15,19 @@ describe 'Previous Page' do
   it { page_state.should validate_presence_of(:content) }
 
   it "should return the correct title" do
-    page_state.title.should == page.original_title
+    page_state.title.should == 'Title'
   end
 
 
   it "should return the correct content" do
-    page_state.content.should == 'the content'
+    page_state.content.should == 'Content'
   end
 
   it "should return the tags" do
-    page_state.tags.should == 'tag1, tag2'
-    page_state.tags = 'tag3'
-    page_state.tags.should == 'tag3'
+    page_state.tags.should == 't1, t2'
+  end
+
+  it "should return the cotegories" do
+    page_state.categories.should == 'c1, c2'
   end
 end
