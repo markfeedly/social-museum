20e6d72ec3adbe75dfcdf104d83a695dba92ecdf
Mark Van <markfeedly@gmail.com>
2014-06-08 15:42:23 +0100

    added unique page title rspec

--- spec/models/unique_page_titles_n_slugs_spec.rb
+++ spec/models/unique_page_titles_n_slugs_spec.rb
@@ -2,19 +2,14 @@ require 'spec_helper'
 
 describe 'Pages with unique titles' do
 
-  def make_page_with_title (title)
-    user = FactoryGirl.create(:user)
-    page = FactoryGirl.create(:page, original_title: title )
-    page_state = FactoryGirl.create(:page_state, title: page.original_title, content: 'the content',
-                                     user: user, page: page)
+  let(:user) {FactoryGirl.create(:user) }
+  let(:page){ FactoryGirl.create(:page, title: 'first title', user: user, content: 'any' ) }
+  let(:ps){ page.history.last }
+
+  it 'should not save a page with the same title' do
+    Page.create(title: page.title, user: user, content: 'any' ).id.should == nil
   end
 
-  before(:each) do
-    make_page_with_title('Title one')
-    make_page_with_title('Title two')
-    make_page_with_title('Used title')
-    make_page_with_title('Title four')
-  end
 
 
 end
