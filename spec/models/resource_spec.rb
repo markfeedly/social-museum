7bf7d8434b783d4e7a385711c128ff62f45a214e
Mark Van <markfeedly@gmail.com>
2014-06-20 16:39:40 +0100

    duh it works mistake before

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -10,10 +10,23 @@ describe Resource do
   end
 
   it "should have verisons" do
-    resource
-    expect(resource.update(title: 'New Title').versions).to eq 1
+    resource.update(title: 'New Title')
+    p resource.versions
+    expect{ resource.versions }.to eq 1
   end
 
 
 
 end
+
+#<ActiveRecord::Associations::CollectionProxy [
+
+# #<Secretary::Version id: 2, version_number: 2, versioned_type: "Resource",
+#   versioned_id: 1, user_id: nil, description: "Changed Title and Lock version",
+#   object_changes: {"title"=>["Title 1", "New Title"], "lock_version"=>[0, 1]}, created_at: "2014-06-20 15:34:37">,
+
+# #<Secretary::Version id: 1, version_number: 1, versioned_type: "Resource",
+#   versioned_id: 1, user_id: nil, description: "Created Resource #1",
+# object_changes: {"lock_version"=>[nil, 0], "title"=>[nil, "Title 1"], "description"=>[nil, "Description 1"], "url"=>[nil, "http://example.org/resource_1"], "user_id"=>[nil, 1]}, created_at: "2014-06-20 15:34:37">
+
+# ]>
