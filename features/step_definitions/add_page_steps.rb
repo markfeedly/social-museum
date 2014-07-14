ef669294626f861f7a00062cf31ee6c37193a55b
Mark Van <markfeedly@gmail.com>
2014-07-14 15:33:41 +0100

    Removing \'Implementation details\' from tests in add_page_steps.rb \& edited syntax

--- features/step_definitions/add_page_steps.rb
+++ features/step_definitions/add_page_steps.rb
@@ -1,27 +1,28 @@
 Given(/^I am signed in$/) do
   @user = FactoryGirl. build(:user)
 
-  visit '/users/sign_up' # new_user_session_path
-  fill_in 'user_email', :with => @user.email
+  visit new_user_session_path
+
+  fill_in('user_email', :with => @user.email)
   fill_in('user_password', :with => @user.password)
   fill_in('user_password_confirmation', :with => @user.password)
+
   click_button('sign_up_button')
 
-  current_path.should == '/'
   page.should have_content('Sign out')
 end
 
 When(/^I create a page entitled "(.*?)" with content "(.*?)"$/) do |title, content|
   click_link('add_page_link')
-  current_path.should == new_page_path
 
   fill_in('Title', :with => title)
   fill_in('Content', :with => content)
+
   click_button('Create Page')
 end
 
 Then(/^I see the navigation menu$/) do
-  within('.nav .nav') do
+  within('.nav') do
       page.should have_content('New Page')
       page.should have_content('Pages')
       page.should have_content('New Resource')
@@ -31,31 +32,24 @@ Then(/^I see the navigation menu$/) do
 end
 
 Then(/^I can see a page entitled "(.*?)" with content "(.*?)"$/) do |title, content|
-  current_slug = current_path.sub /.*\//, ''
-  expect(current_path).to eq pages_path + '/' + current_slug
-
   page.should have_content(title)
   page.should have_content(content)
 end
 
 When(/^I create a tagged page entitled "(.*?)" with content "(.*?)" and tags (.*?)$/) do |title, content, tags|
   click_link('add_page_link')
-  current_path.should == new_page_path
 
   fill_in('Title',   :with => title)
   fill_in('Content', :with => content)
   fill_in('Tags',    :with => tags)
 
-  click_button('Create Page')end
+  click_button('Create Page')
+end
 
 Then(/^I can see a tagged page entitled "(.*?)" with content "(.*?)" and tags "(.*?)"$/) do |title, content, tags|
-  current_slug = current_path.sub /.*\//, ''
-  current_path.should == pages_path + '/' + current_slug
-
   page.should have_content(title)
   page.should have_content(content)
   page.should have_content(tags)
-
 end
 
 
