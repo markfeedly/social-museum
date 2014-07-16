243f9276a072b5852cffdb0d69e3b166c59e08c8
Mark Van <markfeedly@gmail.com>
2014-07-16 14:50:12 +0100

    refactored link output spec to include a helper method

--- spec/parsers/link_interpreter_output_spec.rb
+++ spec/parsers/link_interpreter_output_spec.rb
@@ -2,67 +2,75 @@ require 'spec_helper'
 
 describe LinkInterpreter do
 
+  def check_outputs(link_interpreter, li_method, html_output)
+    expect( link_interpreter.send(li_method)).to eq html_output
+    expect( link_interpreter.process).to eq html_output
+  end
+
   it "should output a hyperlink to an existing page" do
     @page = FactoryGirl.create(:page)
     li=LinkInterpreter.new(@page.title)
-    li.process_page_title.should ==
-        "<a href='/pages/#{@page.title.downcase.gsub(/ /, '-')}' data-page>#{@page.title}</a>"
-    li.process.should ==
-        "<a href='/pages/#{@page.title.downcase.gsub(/ /, '-')}' data-page>#{@page.title}</a>"
+
+    check_outputs(li, :process_page_title,
+        "<a href='/pages/#{@page.title.downcase.gsub(/ /, '-')}' data-page>#{@page.title}</a>" )
+  end
+
+
+
+  it "should output a hyperlink to an existing page" do
+    @page = FactoryGirl.create(:page)
+    li=LinkInterpreter.new(@page.title)
+
+    check_outputs(li, :process_page_title,
+        "<a href='/pages/#{@page.title.downcase.gsub(/ /, '-')}' data-page>#{@page.title}</a>" )
   end
 
   it "should output a hyperlink to a missing page" do
     @page = FactoryGirl.create(:page)
     new_page_title = "This is not the title of existing page #{@page.title}; rather for a page to be created by a user"
     li=LinkInterpreter.new(new_page_title)
-    li.process_page_title.should ==
-        "<a href='/pages/new?page_title=#{title.to_param}' data-new-page>#{title}</a>"
-    li.process.should ==
-        "<a href='/pages/new?page_title=#{title.to_param}' data-new-page>#{title}</a>"
+
+    check_outputs(li, :process_page_title,
+        "<a href='/pages/new?page_title=#{new_page_title.to_param}' data-new-page>#{new_page_title}</a>" )
   end
 
   it "should output a hyperlink to a URL with no link text" do
     li = LinkInterpreter.new('http://hedtek.com')
-    li.process.should ==
-        "<a href='http://hedtek.com' external-link>http://hedtek.com</a>"
-    li.process.should ==
-        "<a href='http://hedtek.com' external-link>http://hedtek.com</a>"
+
+    check_outputs(li, :process_url_without_text,
+        "<a href='http://hedtek.com' external-link>http://hedtek.com</a>"  )
   end
 
-  it "should output a hyperlink to a URL link text" do
+  it "should output a hyperlink to a URL with ther URL as the link text" do
     url = 'http://hedtek.com/some/page'
     li = LinkInterpreter.new(url)
-    li.process_url_without_text.should ==
-        "<a href='#{url}' external-link>#{url}</a>"
-    li.process.should ==
-        "<a href='#{url}' external-link>#{url}</a>"
+
+    check_outputs(li, :process_url_without_text,
+        "<a href='#{url}' external-link>#{url}</a>" )
   end
 
   it "should output a hyperlink showing text" do
     url_and_hyperlink_with_text= 'http://hedtek.com/some/page funny money'
     li = LinkInterpreter.new(url_and_hyperlink_with_text)
-    li.process_url_with_text.should ==
-        "<a href='http://hedtek.com/some/page' external-link>funny money</a>"
-    li.process.should ==
-        "<a href='http://hedtek.com/some/page' external-link>funny money</a>"
+
+    check_outputs(li, :process_url_with_text,
+        "<a href='http://hedtek.com/some/page' external-link>funny money</a>" )
   end
 
   it "should process an unsized image" do
     image_url= 'http://hedtek.com/some/page.png'
     li = LinkInterpreter.new(image_url)
-    li.process_image_url_without_width.should ==
-        "<div><img src='#{image_url}'/></div>"
-    li.process.should ==
-        "<div><img src='#{image_url}'/></div>"
+
+    check_outputs(li, :process_image_url_without_width,
+        "<div><img src='#{image_url}'/></div>" )
   end
 
   it "should process an image with a display width in pixels" do
     image_url_and_width= 'http://hedtek.com/some/page.png  300'
     li=LinkInterpreter.new(image_url_and_width)
-    li.process_image_url_with_width.should ==
-        "<div><img src='http://hedtek.com/some/page.png' style='width: 300px;'/></div>"
-    li.process.should ==
-        "<div><img src='http://hedtek.com/some/page.png' style='width: 300px;'/></div>"
+
+    check_outputs(li, :process_image_url_with_width,
+        "<div><img src='http://hedtek.com/some/page.png' style='width: 300px;'/></div>" )
   end
 
 end
