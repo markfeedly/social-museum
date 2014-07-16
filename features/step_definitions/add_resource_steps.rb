5fc0794c9ea8783e95e4d6b9e5c671b6d10b6ef0
Charles Reynolds <reynoldscem@gmail.com>
2014-07-16 14:47:08 +0100

    Added more resource tests

---
+++ features/step_definitions/add_resource_steps.rb
@@ -0,0 +1,16 @@
+When(/^I create a new resource with title "(.*?)" and link of "(.*?)"$/) do |title, resource_url|
+  visit new_resource_path
+
+  fill_in('resource_title', :with => title)
+  fill_in('resource_url', :with => resource_url)
+  click_button('Create Resource')
+end
+
+When(/^I create a new resource with title "(.*?)" and a link of "(.*?)", associated with page "(.*?)"$/) do |resource_title, resource_url, page_title|
+  visit new_resource_path
+
+  fill_in('resource_title', :with => resource_title)
+  fill_in('resource_url', :with => resource_url)
+  check('resource_pages_' + page_title)
+  click_button('Create Resource')
+end
