5ec2d903046a3425f04c8ae8df591feab06d48b9
Charles Reynolds <reynoldscem@gmail.com>
2014-07-17 15:21:48 +0100

    Added resource test

--- features/step_definitions/view_resource_steps.rb
+++ features/step_definitions/view_resource_steps.rb
@@ -1,11 +1,11 @@
-Then(/^I can see an image resource with title "(.*?)" and link to "(.*?)"$/) do |resource_title, resource_url|
+Then(/^I can see an image resource with title "(.*?)", and a link to "(.*?)"$/) do |resource_title, resource_url|
   visit resources_path
 
   page.should have_content(resource_title)
   page.should have_css("img[src='#{resource_url}']")
 end
 
-Then(/^I can see a resource with title "(.*?)" and a link to "(.*?)", associated with page "(.*?)"$/) do |resource_title, resource_url, page_title |
+Then(/^I can see a resource with title "(.*?)", and a link to "(.*?)", associated with page "(.*?)"$/) do |resource_title, resource_url, page_title |
   page.should have_link(page_title, :href => page_path(Page.find_by_title(page_title)))
 end
 
