d957f7ae1d5c3158b550abe2fb4c3a1e91a1834f
Charles Reynolds <reynoldscem@gmail.com>
2014-07-22 11:32:48 +0100

    Removal of superfluous controller tests, added todo on oauth controller_callback spec, improved cleaning strategy for FactoryGirl - was causing dependency conflicts in resource_spec.

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe OmniauthCallbacksController do
 
+  # TODO Finish omniauth testing
   before :each do
     request.env["devise.mapping"] = Devise.mappings[:user]
     request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
