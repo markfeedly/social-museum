43815d0975838def745e63286503c277af332405
Charles Reynolds <reynoldscem@gmail.com>
2014-07-24 15:40:33 +0100

    Minor refactor of page_state_spec

--- spec/models/page_state_spec.rb
+++ spec/models/page_state_spec.rb
@@ -13,18 +13,18 @@ describe 'Previous Page' do
   it { page_state.should belong_to(:user) }
 
   it "should return the correct title" do
-    page_state.title.should == 'Title'
+    expect(page_state.title).to eq 'Title'
   end
 
   it "should return the correct content" do
-    page_state.content.should == 'Content'
+    expect(page_state.content).to eq 'Content'
   end
 
   it "should return the tags" do
-    page_state.tags.should == 't1, t2'
+    expect(page_state.tags).to eq 't1, t2'
   end
 
   it "should return the categories" do
-    page_state.categories.should == 'c1, c2'
+    expect(page_state.categories).to eq 'c1, c2'
   end
 end
