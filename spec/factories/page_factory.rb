64cedc536e245491f90b6c7906b5217afc9d0c94
Charles Reynolds <reynoldscem@gmail.com>
2014-07-24 15:28:31 +0100

    Whitespace...

--- spec/factories/page_factory.rb
+++ spec/factories/page_factory.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
+
   factory :page do
+
     sequence(:title) {|n| "Post Title #{n}" }
     sequence(:content) {|n| "Post content #{n}" }
+
     user
   end
 end
--- spec/factories/resource_factory.rb
+++ spec/factories/resource_factory.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
+
   factory :resource do
+
     sequence(:title) {|n| "Title #{n}" }
     sequence(:description) {|n| "Description #{n}" }
     sequence(:url) {|n| "http://example.org/resource_#{n}" }
+
     user
   end
 end
