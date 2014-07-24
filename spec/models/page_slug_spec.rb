7f0e06f4e8975e64152d2928ed9eb19ac0f221cc
Charles Reynolds <reynoldscem@gmail.com>
2014-07-24 15:34:52 +0100

    Stylistic changes accross spec/models for readability

--- spec/models/page_slug_spec.rb
+++ spec/models/page_slug_spec.rb
@@ -3,15 +3,21 @@ require 'spec_helper'
 describe 'Page slug' do
 
   let(:user) {FactoryGirl.create(:user) }
-  let(:page){ FactoryGirl.create(:page, title: 'first title', user: user, content: 'any' ) }
+  let(:page){ FactoryGirl.create(:page,
+                                 title: 'first title',
+                                 user: user,
+                                 content: 'any' ) }
 
   it "should make a page slug" do
-  expect(page.slug).to eq 'first-title'
+    expect(page.slug).to eq 'first-title'
   end
 
   it "should downcase slug" do
-    expect(FactoryGirl.create(:page, title: 'FirSt Title 1', user: user, content: 'any' ).slug).
-      to eq 'first-title-1'
+    expect(FactoryGirl.create(:page,
+                              title: 'FirSt Title 1',
+                              user: user,
+                              content: 'any' ).
+           slug).to eq 'first-title-1'
   end
 end
 
--- spec/models/unique_page_titles_n_slugs_spec.rb
+++ spec/models/unique_page_titles_n_slugs_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 describe 'Pages with unique titles' do
 
   let(:user) {FactoryGirl.create(:user) }
-  let(:page){ FactoryGirl.create(:page, title: 'first title', user: user, content: 'any' ) }
+  let(:page){ FactoryGirl.create(:page,
+                                 title: 'first title',
+                                 user: user,
+                                 content: 'any' ) }
 
   it 'should not save a page with the same title' do
-    expect{Page.create!(title: page.title, user: user, content: 'any' )}.
+    expect{Page.create!(title: page.title,
+                        user: user,
+                        content: 'any' )}.
       to raise_error(ActiveRecord::RecordInvalid,'Validation failed: Title has already been taken, Slug has already been taken')
   end
 
