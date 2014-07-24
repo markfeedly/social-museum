99bd00c1383ec928ac523a209c3bcfb08a5b706b
Charles Reynolds <reynoldscem@gmail.com>
2014-07-24 11:44:26 +0100

    Modified content_html_genrator_spec to have more appropriate tests

--- spec/parsers/content_html_generator_spec.rb
+++ spec/parsers/content_html_generator_spec.rb
@@ -6,51 +6,53 @@ describe ContentHtmlGenerator do
   let(:page) { FactoryGirl.create(:page, user: user) }
 
   it "should process plain text correctly" do
-    expect(ContentHtmlGenerator.generate_full(page)).to eq "<p>#{page.content}</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<p>#{page.content}</p>"
   end
 
   it "should process an existing page title correctly" do
     page.update(user: user, content: "preamble [#{page.title}] postamble")
 
-    expect(ContentHtmlGenerator.generate_full(page)).to eq \
-        "<p>preamble <a href='/pages/#{page.slug}' data-page>#{page.title}</a> postamble</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<a href='/pages/#{page.slug}' data-page>#{page.title}</a>"
   end
 
   it "should process a non-existing page title" do
     page.update(user: user, content: "pre-amble [My non-existent title] post-amble")
 
-    expect(ContentHtmlGenerator.generate_full(page)).to eq \
-        "<p>pre-amble <a href='/pages/new?page_title=My non-existent title' data-new-page>My non-existent title</a> post-amble</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<a href='/pages/new?page_title=My non-existent title' data-new-page>My non-existent title</a>"
   end
 
   it "should process an embedded image" do
-    page.update(user: user, content: 'pre-amble [http://a.b/img.png] post-amble')
+    page.update(user: user, content: "pre-amble [http://a.b/img.png] post-amble")
 
-    ContentHtmlGenerator.generate_full(page).should ==
-        "<p>pre-amble <img src='http://a.b/img.png'/> post-amble</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<img src='http://a.b/img.png'/>"
   end
 
   it "should process an embedded image and width" do
-    #pending 'look into what should really be output in tricky circumstances'
-    page.update(user: user, content: 'pre-amble [http://a.b/img.png 100] post-amble')
+    page.update(user: user, content: "pre-amble [http://a.b/img.png 100] post-amble")
 
-    ContentHtmlGenerator.generate_full(page).should ==
-        "<p>pre-amble <img src='http://a.b/img.png' style='width: 100px;'/> post-amble</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<img src='http://a.b/img.png' style='width: 100px;'/>"
   end
 
   it "should process an embedded image and width in tricky circumstances" do
-    #pending 'look into what should really be output re paras and divs'
-    page.update(user: user, content: '[http://a.b/first.png 100] some text [http://a.b/second.png 100] post-amble')
+    page.update(user: user, content: "[http://a.b/first.png 100] some text [http://a.b/second.png 100] post-amble")
 
-    ContentHtmlGenerator.generate_full(page).should ==
-        "<p><img src='http://a.b/first.png' style='width: 100px;'/> some text <img src='http://a.b/second.png' style='width: 100px;'/> post-amble</p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<img src='http://a.b/first.png' style='width: 100px;'/>"
+
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<img src='http://a.b/second.png' style='width: 100px;'/>"
   end
 
   it "should process an only on line image and width" do
-    page.update(user: user, content: '[http://a.b/img.png 100]')
+    page.update(user: user, content: "[http://a.b/img.png 100]")
 
-    ContentHtmlGenerator.generate_full(page).should ==
-        "<p><img src='http://a.b/img.png' style='width: 100px;'/></p>\n"
+    expect(ContentHtmlGenerator.generate_full(page)).to include
+        "<img src='http://a.b/img.png' style='width: 100px;'/>"
   end
 
 end
