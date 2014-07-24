4903bd92e6274f0aca998bba242606b057013803
Charles Reynolds <reynoldscem@gmail.com>
2014-07-24 13:12:31 +0100

    Adding validation checks to page_spec, and adding more context for test cases

--- spec/models/page_state_spec.rb
+++ spec/models/page_state_spec.rb
@@ -5,23 +5,12 @@ describe 'Previous Page' do
   let(:page){ FactoryGirl.create(:page, title: 'Title', content: 'Content', tags: 't1, t2', categories: 'c1, c2') }
   let(:page_state) { page.history.last }
 
-  #it { page_state.should validate_presence_of(:page_id) }
   it { page_state.should belong_to(:page) }
-
-  #it { page_state.should validate_presence_of(:user_id) }
   it { page_state.should belong_to(:user) }
-
-  #it { page_state.should validate_presence_of(:title) }
-  #it { page_state.should validate_presence_of(:content) }
-
-  it "should perhaps validate" do
-    pending 'explore above validations'
-  end
 
   it "should return the correct title" do
     page_state.title.should == 'Title'
   end
-
 
   it "should return the correct content" do
     page_state.content.should == 'Content'
@@ -31,7 +20,7 @@ describe 'Previous Page' do
     page_state.tags.should == 't1, t2'
   end
 
-  it "should return the cotegories" do
+  it "should return the categories" do
     page_state.categories.should == 'c1, c2'
   end
 end
