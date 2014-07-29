50fe17e271d2d1d9c2f17277d16cf813d0b88457
Charles Reynolds <reynoldscem@gmail.com>
2014-07-29 13:13:44 +0100

    Reverting comment files to state they were in prior to adding code for spam filtering. These changes can now be seen in a new branch. It may make sense to merge this branch (update-mail) into master

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,44 +5,10 @@ class CommentsController < ApplicationController
     @comment = @page.comments.create(user: current_user,
                                      commenter: params[:comment][:commenter],
                                      content: params[:comment][:content]  )
-    @comment.request = request
-    if @comment.save
-      if @comment.approved?
-        flash[:notice] = "Comment submited succesfully."
-      else
-        flash[:error]  = "Comment is waiting to be approved by an admin."
-      end
-    end
     redirect_to :back
   end
 
   def index
-    @approved_comments = Comment(:approved => true)
-    @rejected_comments = Comment(:approved => false) if admin?
-  end
-
-  def destroy
-    @comment = Comment.destroy(params[:id])
-    flash[:notice] = "Comment destroyed"
-    redirect_to :back
-  end
-
-  def destroy_multiple
-    comment.destroy(params[:comment_ids])
-    flash[:notice] = "Successfully destroyed comments"
-    redirect_to :back
-  end
-
-  def approve
-    @comment = Comment.find([params[:id]])
-    @comment.mark_as_ham!
-    redirect_to :back
-  end
-
-  def reject
-    @comment = Comment.find([params[:id]])
-    @comment.mark_as_spam!
-    redirect_to :back
   end
 
   def page_params
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,53 +1,22 @@
 class Comment < ActiveRecord::Base
   belongs_to :page
   belongs_to :user
-  validates_presence_of :content, :commenter
-  before_create :check_for_spam
+  validates :content, presence: true
+  # validates :commenter, presence: true
+  #TODO add error feedback if either of the two above are true
+
   after_create :subscribe_creator, :notify_subscribers
 
-  def request=(request)
-    self.user_ip    = request.remote_ip
-    self.user_agent = request.env["HTTP_USER_AGENT"]
-    self.referrer   = request.env["HTTP_REFERRER"]
-  end
+  private
 
-  def check_for_spam
-    self.approved = !Akismetor.spam?(akismet_attributes)
-    true
-  end
-
-  def akismet_attributes
-    {
-        :key                  => ENV['WORDPRESS_KEY'],
-        :blog                 => root_path,
-        :user_ip              => user_ip,
-        :user_agent           => user_agent,
-       #:comment_author       => commenter, # How can these be derived from attached :commenter
-       #:comment_author_email => email,     # ?
-        :comment_content      => content
-    }
-  end
-
-  def mark_as_spam!
-    update_attribute(:approved, false)
-    Akismetor.submit_spam(akismet_attributes)
-  end
-
-  def mark_as_ham!
-    update_attribute(:approved, true)
-    Akismetor.submit_spam(akismet_attributes)
+  def subscribe_creator
+    page.subscribe(user)
   end
 
   def notify_subscribers
     (page.subscribers - [user]).each do |usr|
       Notifier.comment_updated(self, usr).deliver
     end
-  end
-
-  private
-
-  def subscribe_creator
-    page.subscribe(user)
   end
 
 end
