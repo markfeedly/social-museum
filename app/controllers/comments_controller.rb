a485007921befb110acd969294388b062e79ee9c
Charles Reynolds <reynoldscem@gmail.com>
2014-07-30 18:08:01 +0100

    Spam for comments essentially finished, still some tidying up to be done and cucumber

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,35 @@
 class CommentsController < ApplicationController
 
+  expose(:comment)
+
   def create
-    @page    = Page.find_by_slug(params['page_id'])
-    @comment = @page.comments.create(user:       current_user,
-                                     content:    params[:comment][:content],
-                                     user_ip:    request.remote_ip,
-                                     user_agent: request.env["HTTP_USER_AGENT"],
-                                     referrer:   request.env["HTTP_REFERRER"])
+    Page.find_by_slug(params['page_id']).
+                      comments.create(user:       current_user,
+                                      content:    params[:comment][:content],
+                                      user_ip:    request.remote_ip,
+                                      user_agent: request.env["HTTP_USER_AGENT"],
+                                      referrer:   request.env["HTTP_REFERRER"])
     redirect_to :back
+  end
+
+  def destroy
+    authorize_action_for comment
+    comment.destroy
+    redirect_to page_path(params['page_id'])
+  end
+
+  def approve
+    authorize_action_for comment
+    comment.ham!
+    comment.save
+    redirect_to page_path(params['page_id'])
+  end
+
+  def disapprove
+    authorize_action_for comment
+    comment.spam!
+    comment.save
+    redirect_to page_path(params['page_id'])
   end
 
   def index
