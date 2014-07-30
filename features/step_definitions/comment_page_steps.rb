67fc06ce4a8803e0131e33fb94cc4dfc9c1e163b
Charles Reynolds <reynoldscem@gmail.com>
2014-07-30 14:21:43 +0100

    Users can always see their own comments, even if unapproved. If it is unapproved there is a message saying so. Other users can only see approved messages, except for admins which can see all.

--- features/step_definitions/comment_page_steps.rb
+++ features/step_definitions/comment_page_steps.rb
@@ -1,4 +1,4 @@
-When(/^I make the comment "(.*?)" on the page entitled "(.*?)"$/) do |page_comment, page_title|
+When(/^(?:I|another user) makes? the comment "(.*?)" on the page entitled "(.*?)"$/) do |page_comment, page_title|
   visit page_path(Page.find_by_title(page_title))
 
   within("#comments") do
@@ -10,5 +10,57 @@ When(/^I make the comment "(.*?)" on the page entitled "(.*?)"$/) do |page_comme
       fill_in("comment_content", with: page_comment)
       click_button("Add comment")
     end
+  end
+end
+
+When(/^a spammer makes? the comment "(.*?)" on the page entitled "(.*?)"$/) do |page_comment, page_title|
+  user_name = User.find_by_email(user_email).name if user_email
+  sign_out
+  create_user("viagra-test-123")
+  sign_in("viagra-test-123")
+
+  visit page_path(Page.find_by_title(page_title))
+
+  within("#comments") do
+    within("#comment-button") do
+      click_link("Add a comment")
+    end
+
+    within("#comment-form") do
+      fill_in("comment_content", with: page_comment)
+      click_button("Add comment")
+    end
+  end
+
+  sign_out
+
+  if user_name
+    sign_in(user_name)
+  end
+end
+
+Then(/^I can see a comment "(.*?)" as (#{CAPTURE_ITEM_RECENCY}) comment on the page entitled "(.*?)"$/) do |comment_content, index, page_title|
+  visit page_path(Page.find_by_title(page_title))
+
+  within("#comments") do
+    within("li[data-comment-idx='#{index}']") do
+      page.should have_content(comment_content)
+    end
+  end
+end
+
+Then(/^I can see a (?:comment|message) "(.*?)" on the page entitled "(.*?)"$/) do |content, page_title|
+  visit page_path(Page.find_by_title(page_title))
+
+  within("#comments") do
+    page.should have_content(content)
+  end
+end
+
+Then(/^I can(?:no|')t see a (?:comment|message) "(.*?)" on the page entitled "(.*?)"$/) do |content, page_title|
+  visit page_path(Page.find_by_title(page_title))
+
+  within("#comments") do
+    page.should_not have_content(content)
   end
 end
--- features/step_definitions/view_page_steps.rb
+++ features/step_definitions/view_page_steps.rb
@@ -39,27 +39,10 @@ Then(/^I cannot see the remove page link$/) do
   page.should_not have_css("[delete-page]")
 end
 
-CAPTURE_ITEM_RECENCY = Transform /^(|second|third) ?most recent$/ do |position|
-  index = 0 if position == ""
-  index = 1 if position == "second"
-  index = 2 if position == "third"
-  index
-end
-
 Then(/^I can see one item of page history containing "(.*?)" as (#{CAPTURE_ITEM_RECENCY})$/) do |arg1, index|
   within('#history-tab') do
     within("li[data-history-idx='#{index}']") do
       page.should have_content(arg1)
-    end
-  end
-end
-
-Then(/^I can see a comment "(.*?)" as (#{CAPTURE_ITEM_RECENCY}) comment on the page entitled "(.*?)"$/) do |comment_content, index, page_title|
-  visit page_path(Page.find_by_title(page_title))
-
-  within("li[data-comment-idx='#{index}']") do
-    within(".comment-content") do
-      page.should have_content(comment_content)
     end
   end
 end
