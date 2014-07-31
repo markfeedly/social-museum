8ea2fcee378f555e61351a50e5a0023eb1e73d37
Charles Reynolds <reynoldscem@gmail.com>
2014-07-31 15:06:04 +0100

    Refactoring out deprecated code

--- features/step_definitions/page_comments_admin_steps.rb
+++ features/step_definitions/page_comments_admin_steps.rb
@@ -2,10 +2,8 @@ When(/^I (approve|disapprove|remove) a comment on the page entitled "([^"]*)"$/)
   visit page_path(Page.find_by_title(page_title))
 
   within("#comments") do
-    within(".comment-content") do
-      if page.has_css?("a[class='#{action}-comment']")
-        page.first("a[class='#{action}-comment']").click
-      end
+    if page.has_css?("a[class='#{action}-comment']")
+      page.first("a[class='#{action}-comment']").click
     end
   end
 end
