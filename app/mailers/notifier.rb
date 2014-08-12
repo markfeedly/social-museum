2ce1dbf302f715092d1a92256b483fad417c2314
Charles Reynolds <reynoldscem@gmail.com>
2014-08-12 16:17:14 +0100

    Name of museum and description now dynamically configurable by Environment variables in application.yml

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,7 +6,7 @@ class Notifier < ActionMailer::Base
     @page    = comment.page
     @user    = user
     mail(:to      => user.email,
-         :subject => "[UMCS] New comment on page #{comment.page.title} at #{comment.created_at}")
+         :subject => "[#{ENV['MUSEUM_NAME']}] New comment on page #{comment.page.title} at #{comment.created_at}")
     #TODO should there be a global variable for the name of the system? Spit out in titles, emails etc
   end
 end
