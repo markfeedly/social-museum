75d8482d19d10d0fd63c92a9c98f203907f65304
David Workman <workmad3@gmail.com>
2014-08-14 10:23:23 +0100

    Keeping .ruby-version and Gemfile in-step and stopping simplecov from loading all the time

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ require 'rspec/rails'
 require 'email_spec'
 require 'shoulda/matchers'
 require 'simplecov'
-#mvh zeus change --  require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
