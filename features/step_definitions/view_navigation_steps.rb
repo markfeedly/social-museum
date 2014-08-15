b99e726023dc97c98133501c44baa126fba827ac
Charles Reynolds <reynoldscem@gmail.com>
2014-08-15 14:33:04 +0100

    Added seeing collection items / new collection item to navigation feature

--- features/step_definitions/view_navigation_steps.rb
+++ features/step_definitions/view_navigation_steps.rb
@@ -2,12 +2,14 @@ Then(/^I see the basic navigation menu$/) do
   within('.navbar') do
     expect(page).to     have_css("a[href='#{pages_path}']")
     expect(page).to     have_css("a[href='#{resources_path}']")
+    expect(page).to     have_css("a[href='#{collection_items_path}']")
     expect(page).to     have_css("a[href='#{recent_index_path}']")
     expect(page).to     have_css("a[href='#{new_user_registration_path}']")
     expect(page).to     have_css("a[href='#{new_user_session_path}']")
 
     expect(page).to_not have_css("a[href='#{new_page_path}']")
     expect(page).to_not have_css("a[href='#{new_resource_path}']")
+    expect(page).to_not have_css("a[href='#{new_collection_item_path}']")
     expect(page).to_not have_css("a[href='#{users_path}']")
     expect(page).to_not have_css("a[href='#{edit_user_registration_path}']")
     expect(page).to_not have_css("a[href='#{destroy_user_session_path}']")
@@ -18,6 +20,7 @@ Then(/^I see the user navigation menu$/) do
   within('.navbar') do
     expect(page).to     have_css("a[href='#{pages_path}']")
     expect(page).to     have_css("a[href='#{resources_path}']")
+    expect(page).to     have_css("a[href='#{collection_items_path}']")
     expect(page).to     have_css("a[href='#{recent_index_path}']")
     expect(page).to     have_css("a[href='#{new_page_path}']")
     expect(page).to     have_css("a[href='#{new_resource_path}']")
@@ -25,6 +28,7 @@ Then(/^I see the user navigation menu$/) do
     expect(page).to     have_css("a[href='#{destroy_user_session_path}']")
 
     expect(page).to_not have_css("a[href='#{users_path}']")
+    expect(page).to_not have_css("a[href='#{new_collection_item_path}']")
     expect(page).to_not have_css("a[href='#{new_user_registration_path}']")
     expect(page).to_not have_css("a[href='#{new_user_session_path}']")
   end
@@ -34,9 +38,11 @@ Then(/^I see the admin navigation menu$/) do
   within('.navbar') do
     expect(page).to     have_css("a[href='#{pages_path}']")
     expect(page).to     have_css("a[href='#{resources_path}']")
+    expect(page).to     have_css("a[href='#{collection_items_path}']")
     expect(page).to     have_css("a[href='#{recent_index_path}']")
     expect(page).to     have_css("a[href='#{new_page_path}']")
     expect(page).to     have_css("a[href='#{new_resource_path}']")
+    expect(page).to     have_css("a[href='#{new_collection_item_path}']")
     expect(page).to     have_css("a[href='#{users_path}']")
     expect(page).to     have_css("a[href='#{edit_user_registration_path}']")
     expect(page).to     have_css("a[href='#{destroy_user_session_path}']")
