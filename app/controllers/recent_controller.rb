c0739f60c2863fc10107bbe159928dbc2acc4d37
Mark Van <markfeedly@gmail.com>
2014-08-30 09:40:06 +0100

    fixed ruby-version by adding strip to gemfile

--- app/controllers/recent_controller.rb
+++ app/controllers/recent_controller.rb
@@ -1,7 +1,7 @@
 class RecentController < ApplicationController
-  expose (:recently_created_pages){Page.order('created_at DESC').limit(10)}
-  expose (:recently_edited_pages){Page.order('updated_at DESC').limit(10)}
-  expose (:recently_created_comments){Comment.order('created_at DESC').limit(10)}
+  expose (:recently_created_pages){Page.order('created_at DESC').limit(40)}
+  expose (:recently_edited_pages){Page.order('updated_at DESC').limit(40)}
+  expose (:recently_created_comments){Comment.order('created_at DESC').limit(40)}
 
   def index
   end
