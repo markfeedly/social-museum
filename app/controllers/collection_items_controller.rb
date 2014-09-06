5d1cb4ef63c2d2b7eb25cad3fe896bff01f48688
Mark Van <markfeedly@gmail.com>
2014-09-06 20:28:10 +0100

    admin can create collection items via ui

--- app/controllers/collection_items_controller.rb
+++ app/controllers/collection_items_controller.rb
@@ -1,53 +1,10 @@
 class CollectionItemsController < ApplicationController
   respond_to :html
 
-  expose(:collection_item, attributes: :collection_item_params, finder: :find_by_slug)
-  #expose(:collection_items) { Page.all.reject {|p| p.history.last.item_number==nil } }
   expose(:page_summaries) { Kaminari.paginate_array(Page.joins(:page_title).order('titles.title ASC').reject {|p| p.history.last.item_number==nil }).page(params[:page_summaries]).per(10) }
-
-  expose(:collection_item_states) do
-    Kaminari.paginate_array(collection_item.load_versions).page(params[:page_ci]).per(10)
-  end
-
-  authorize_actions_for CollectionItem
-
-  def show
-    respond_with(collection_item)
-  end
-
-  def new
-    collection_item.build_title
-    respond_with(collection_item)
-  end
-
-  def create
-    collection_item.save
-    respond_with(collection_item)
-  end
-
-  def edit
-    respond_with(collection_item)
-  end
-
-  def update
-    collection_item.update_attributes(collection_item_params)
-    respond_with(collection_item)
-  end
 
   def index
     respond_with(page_summaries)
   end
 
-  def destroy
-    collection_item.destroy
-    respond_with(collection_item)
-  end
-
-  private
-
-  def collection_item_params
-    params.require(:collection_item).permit(:description,
-                                            :item_number, :location,
-                                            title_attributes: [:title, :id])
-  end
 end
