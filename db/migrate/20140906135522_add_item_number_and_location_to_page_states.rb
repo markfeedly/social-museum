e6b6d33a8d1ed89de66ff7fc00b45a350b9e7836
Mark Van <markfeedly@gmail.com>
2014-09-07 08:47:26 +0100

    migration from previously, not added previously

---
+++ db/migrate/20140906135522_add_item_number_and_location_to_page_states.rb
@@ -0,0 +1,6 @@
+class AddItemNumberAndLocationToPageStates < ActiveRecord::Migration
+  def change
+    add_column :page_states, :item_number, :string
+    add_column :page_states, :location, :string
+  end
+end
