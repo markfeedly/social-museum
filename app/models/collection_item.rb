6f00a07a2925334ae3aebd9e295b26c39d5a8465
Mark Van <markfeedly@gmail.com>
2014-09-15 00:06:03 +0100

    seemingly now works creating collection items but still  WITHOUT CREATOR SUBSCRIPTION

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -61,4 +61,8 @@ class CollectionItem < ActiveRecord::Base
   def self.find_by_slug(slug)
     joins(:title).where(titles: {slug: slug}).first
   end
+
+  def self.find_by_title(title)
+    joins(:title).where(titles: {title: title}).first
+  end
 end
