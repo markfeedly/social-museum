f30032a1f0d028ccc60d5eec112d47d22bbf560e
Mark van Harmelen <markvanharmelen@gmail.com>
2014-09-15 22:03:13 +0100

    implemented history_attrs_from classname in module HistoryControl
     PageState appears to work without PageState#user= thanks to a change in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   respond_to :html
   expose(:page, attributes: :page_params, finder: :find_by_slug) {|default| default.decorate }
   expose(:pages)
-  expose(:page_states) { page.history.page(params[:page_states]).per(5) }
+  expose(:page_states) { Kaminari.paginate_array(page.history.to_a.reverse).page(params[:page_states]).per(5) }
   expose(:page_summaries) { pages.joins(:page_title).order('titles.title ASC').page(params[:page_summaries]).per(10) }
 
   before_action :authenticate_user!, :except => [:index, :show]
@@ -16,7 +16,7 @@ class PagesController < ApplicationController
   end
 
   def create
-    page.user = current_user
+    page.user_id = current_user.id
     page.save
     respond_with(page)
   end
