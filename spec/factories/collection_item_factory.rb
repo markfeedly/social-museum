b924037a3f04cfa284ef39d2fe0bea02d39db4b4
mbassmvh <markvh@manchester.ac.uk>
2016-05-23 22:29:07 +0100

    refactor tests for collection item categories, add title to collection item factory

--- spec/factories/collection_item_factory.rb
+++ spec/factories/collection_item_factory.rb
@@ -1,8 +1,10 @@
 FactoryGirl.define do
   factory :collection_item do
+    title
     user_id      1
     location     "LF display case"
     sequence(:item_number) { |n| "#{n}" }
+    sequence(:description) { |n| "Some description #{n}" }
     lock_version  0
     after(:create) do |instance|
       instance.set_categories_from_string('cat1,cat2')
