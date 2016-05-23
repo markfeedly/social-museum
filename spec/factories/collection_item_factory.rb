b8745a87f67e145791e569365b9677ba9ba843e6
mbassmvh <markvh@manchester.ac.uk>
2016-05-23 22:11:55 +0100

    refactor tests for collection item tags

--- spec/factories/collection_item_factory.rb
+++ spec/factories/collection_item_factory.rb
@@ -4,7 +4,10 @@ FactoryGirl.define do
     location     "LF display case"
     sequence(:item_number) { |n| "#{n}" }
     lock_version  0
-    after(:create) {|instance| instance.set_categories_from_string('cat1,cat2') }
+    after(:create) do |instance|
+      instance.set_categories_from_string('cat1,cat2')
+      instance.set_tags_from_string( 'tag2, tag1' )
+    end
   end
 end
 
