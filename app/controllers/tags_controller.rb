9ca80093ecfae4e5f59690b77becbda8940c2606
mbassmvh <markvh@manchester.ac.uk>
2016-05-29 14:37:46 +0100

    decent exposure for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,5 +2,19 @@ class TagsController < ApplicationController
 
   expose(:paginated_pages) { Tag.where(name: params[:id]).first.pages}
 
+  expose(:tag_is) { Tag.where(name: params[:id]).first }
+
+  expose(:tagged_collection_items) { Kaminari.paginate_array( (Title.all.select{|t| t.titleable.has_tag?(params[:id]) && t.titleable.class == CollectionItem ? t.titleable : nil }.collect{|t| t.titleable} )).page(params[:page]).per(10) }
+  expose(:tagged_pages)            { Kaminari.paginate_array( (Title.all.select{|t| t.titleable.has_tag?(params[:id]) && t.titleable.class == Page ? t.titleable : nil }.collect{|t| t.titleable} )).page(params[:page]).per(10) }
+  expose(:tagged_resources)        { Kaminari.paginate_array( (Title.all.select{|t| t.titleable.has_tag?(params[:id]) && t.titleable.class == Resource   ? t.titleable : nil }.collect{|t| t.titleable} )).page(params[:page]).per(10) }
+
+  expose(:ci_count) { tag_is.collection_items.count }
+  expose(:p_count)  { tag_is.pages.count }
+  expose(:r_count)  { tag_is.resources.count }
+  expose(:none)     { (ci_count + p_count + r_count ) == 0 }
+
+  expose(:ci_class) { ci_count > 0 ? 'active' : '' }
+  expose(:p_class)  { ci_count == 0 && p_count > 0 ? 'active' : '' }
+  expose(:r_class)  { ci_count == 0 && p_count == 0 && r_count > 0 ? 'active' : '' }
 
 end
