133a05f0e373342892577241a2e761e5e8f74278
mbassmvh <markvh@manchester.ac.uk>
2016-05-29 13:26:48 +0100

    unrefactored tabbed display for CollectionItems, Pages and Resources on tagged as page

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ActiveRecord::Base
   has_many :tag_items
   has_many :collection_items, through: :tag_items, source: :taggable, source_type: 'CollectionItem'
   has_many :pages, through: :tag_items, source: :taggable, source_type: 'Page'
+  has_many :resources, through: :tag_items, source: :taggable, source_type: 'Resource'
+
 
   before_destroy {collection_items.clear; pages.clear}
 end
