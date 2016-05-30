baad1574fb29071deb4aab54feafd063176de8e6
mbassmvh <markvh@manchester.ac.uk>
2016-05-30 14:38:29 +0100

    spurious commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,15 +3,17 @@ class CategoriesController < ApplicationController
   expose(:category_is) { Category.where(name: params[:id]).first }
 
   expose(:categorised_collection_items) do
-    collection = category_is ? CollectionItem.all.select { |ci| ci.in_category_and_its_children?(params[:id]) ? ci : nil } : []
+    collection = category_is ? category_is.collection_items.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
     Kaminari.paginate_array(collection).page(params[:page]).per(10)
   end
   expose(:categorised_pages) do
-    collection = category_is ? category_is.pages.select { |ci| ci.in_category_and_its_children?(params[:id]) ? ci : nil } : []
+    collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
+    #collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
     Kaminari.paginate_array(collection).page(params[:page]).per(10)
   end
   expose(:categorised_resources) do
-    collection = category_is ? category_is.resources.select { |ci| ci.in_category_and_its_children?(params[:id]) ? ci : nil } : []
+    collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
+    #collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
     Kaminari.paginate_array(collection).page(params[:page]).per(10)
   end
 
@@ -23,6 +25,20 @@ class CategoriesController < ApplicationController
   expose(:ci_class) { ci_count > 0 ? 'active' : '' }
   expose(:p_class)  { ci_count == 0 && p_count > 0 ? 'active' : '' }
   expose(:r_class)  { ci_count == 0 && p_count == 0 && r_count > 0 ? 'active' : '' }
+  
+=begin
+   collection = category_is ? category_is.collection_items.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
+     Kaminari.paginate_array(collection).page(params[:page]).per(10)
+   end
+   expose(:categorised_pages) do
+     collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
+     Kaminari.paginate_array(collection).page(params[:page]).per(10)
+   end
+   expose(:categorised_resources) do
+     collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
+     Kaminari.paginate_array(collection).page(params[:page]).per(10)
+   end
+=end
 
 
 end
