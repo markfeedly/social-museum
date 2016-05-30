875d89eaf513c55722a9824d6b25d65ef0539427
mbassmvh <markvh@manchester.ac.uk>
2016-05-30 16:40:16 +0100

    added show full for categories, still need to bind it in at the UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,44 +1,46 @@
 class CategoriesController < ApplicationController
-  
+
   expose(:category_is) { Category.where(name: params[:id]).first }
+  expose(:all_category_is) { Category.where(name: params[:category_id]).first }
 
-  expose(:categorised_collection_items) do
-    collection = category_is ? category_is.collection_items.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-    Kaminari.paginate_array(collection).page(params[:page]).per(10)
-  end
-  expose(:categorised_pages) do
-    collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-    #collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-    Kaminari.paginate_array(collection).page(params[:page]).per(10)
-  end
-  expose(:categorised_resources) do
-    collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-    #collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-    Kaminari.paginate_array(collection).page(params[:page]).per(10)
-  end
+  expose(:all_categorised_collection_items) { all_category_is ? CollectionItem.all.select { |ci| ci.in_category_and_its_children?(params[:category_id]) ? ci : nil } : [] }
+  expose(:paginated_all_collection_items) { Kaminari.paginate_array(all_categorised_collection_items).page(params[:page]).per(10) }
+  expose(:all_ci_count) { all_category_is ? all_categorised_collection_items.count : 0 }
 
+  expose(:categorised_collection_items) { category_is ? category_is.collection_items.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : [] }
+  expose(:paginated_caegorised_collection_items) { Kaminari.paginate_array(categorised_collection_items).page(params[:page]).per(10) }
   expose(:ci_count) { category_is ? categorised_collection_items.count : 0 }
-  expose(:p_count)  { category_is ? categorised_collection_items.count : 0 }
-  expose(:r_count)  { category_is ? categorised_resources.count : 0 }
+
+
+  expose(:all_categorised_pages) { all_category_is ? Page.all.select { |ci| ci.in_category_and_its_children?(params[:category_id]) ? ci : nil } : [] }
+  expose(:paginated_all_categorised_pages) { Kaminari.paginate_array(all_categorised_pages).page(params[:page]).per(10) }
+  expose(:all_p_count) { all_category_is ? all_categorised_pages.count : 0 }
+
+  expose(:categorised_pages) { category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : [] }
+  expose(:paginated_categorised_pages) { Kaminari.paginate_array(categorised_pages).page(params[:page]).per(10) }
+  expose(:p_count) { category_is ? categorised_pages.count : 0 }
+
+  expose(:all_categorised_resources) { all_category_is ? Resource.all.select { |ci| ci.in_category_and_its_children?(params[:category_id]) ? ci : nil } : [] }
+  expose(:paginated_all_resources) { Kaminari.paginate_array(all_categorised_resources).page(params[:page]).per(10) }
+  expose(:all_r_count) { all_category_is ? all_categorised_resources.count : 0 }
+
+  expose(:categorised_resources) { category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : [] }
+  expose(:paginated_categorised_resources) { Kaminari.paginate_array(categorised_resources).page(params[:page]).per(10) }
+  expose(:r_count) { category_is ? categorised_resources.count : 0 }
+
   expose(:none)     { category_is ? true : false }
+  expose(:all_none)     { all_category_is ? true : false }
+
+  expose(:all_ci_class) { all_ci_count > 0 ? 'active' : '' }
+  expose(:all_p_class)  { all_ci_count == 0 && all_p_count > 0 ? 'active' : '' }
+  expose(:all_r_class)  { all_ci_count == 0 && all_p_count == 0 && all_r_count > 0 ? 'active' : '' }
 
   expose(:ci_class) { ci_count > 0 ? 'active' : '' }
   expose(:p_class)  { ci_count == 0 && p_count > 0 ? 'active' : '' }
   expose(:r_class)  { ci_count == 0 && p_count == 0 && r_count > 0 ? 'active' : '' }
-  
-=begin
-   collection = category_is ? category_is.collection_items.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-     Kaminari.paginate_array(collection).page(params[:page]).per(10)
-   end
-   expose(:categorised_pages) do
-     collection = category_is ? category_is.pages.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-     Kaminari.paginate_array(collection).page(params[:page]).per(10)
-   end
-   expose(:categorised_resources) do
-     collection = category_is ? category_is.resources.select { |ci| ci.has_category?(params[:id]) ? ci : nil } : []
-     Kaminari.paginate_array(collection).page(params[:page]).per(10)
-   end
-=end
 
+  expose(:all_ci_class) { all_ci_count > 0 ? 'active' : '' }
+  expose(:all_p_class)  { all_ci_count == 0 && all_p_count > 0 ? 'active' : '' }
+  expose(:all_r_class)  { all_ci_count == 0 && all_p_count == 0 && all_r_count > 0 ? 'active' : '' }
 
 end
