5c0fb0e145eb681f2909347a92175ad1ced4573b
mbassmvh <markvh@manchester.ac.uk>
2016-06-05 16:24:19 +0100

    comments can only be made if logged in

---
+++ app/authorizers/subscription_authorizer.rb
@@ -0,0 +1,19 @@
+class SubscriptionAuthorizer < Authority::Authorizer
+
+  def self.default(adjective, user)
+    user.admin?
+  end
+
+  def self.creatable_by?(user, opts = {})
+      !user.guest?
+  end
+
+  def deletable_by?(user, opts={})
+    resource.user == user || super
+  end
+
+  def readable_by?(user, opts={})
+    resource.user == user || super
+    false
+  end
+end
