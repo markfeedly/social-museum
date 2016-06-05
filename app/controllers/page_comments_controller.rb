0451c54897a962d702ac1c3179820578a9d289fe
mbassmvh <markvh@manchester.ac.uk>
2016-06-05 14:18:16 +0100

    added todos

--- app/controllers/page_comments_controller.rb
+++ app/controllers/page_comments_controller.rb
@@ -1,10 +1,8 @@
 class PageCommentsController < CommentsController
   expose(:page, finder_parameter: :page_id, finder: :find_by_slug)
+
+  #todo work out if this is needed
   def commentable
     page
-  end
-
-  def after_page_create_path
-    page_path(page)
   end
 end
