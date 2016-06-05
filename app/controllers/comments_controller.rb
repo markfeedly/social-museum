2082a633c8af2f31cdb10df9fbdb2246abe6df22
mbassmvh <markvh@manchester.ac.uk>
2016-06-05 14:18:16 +0100

    added todos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   expose(:comment)
+  expose(:page){Page.find(params[:comment][:commentable_id].to_i)}
 
   def create
     Comment.create( user:   current_user,
@@ -10,7 +11,7 @@ class CommentsController < ApplicationController
                     commentable_id: params[:comment][:commentable_id].to_i,
                     user_agent: request.env["HTTP_USER_AGENT"],
                     referrer:   request.env["HTTP_REFERRER"] )
-    redirect_to page_path(Page.find(params[:comment][:commentable_id].to_i))
+    redirect_to page_path(page)
   end
 
   def destroy
