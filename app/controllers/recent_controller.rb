83c6df6abb5f145b83ec7389791a9ec650580362
mbassmvh <markvh@manchester.ac.uk>
2016-06-06 17:23:36 +0100

    cucumber for some emailing, buttons enabled on subscription overview, frame work for adding resource subscriptions on that page in place

--- app/controllers/recent_controller.rb
+++ app/controllers/recent_controller.rb
@@ -7,9 +7,9 @@ class RecentController < ApplicationController
   expose (:recently_edited_resources){Resource.order('updated_at DESC').limit(500).select{|p|p.created_at!=p.updated_at}[0..39]}
   expose (:recently_created_comments){Comment.order('created_at DESC').limit(40)}
 
-  expose (:recently_created_collection_item_comments){Comment.order('created_at DESC').limit(40)}
-  expose (:recently_created_page_comments){Comment.order('created_at DESC').limit(40)}
-  expose (:recently_created_resource_comments){Comment.order('created_at DESC').limit(40)}
+  expose (:recently_created_collection_item_comments){ Comment.where(approved: true).order('created_at DESC').select{|c| c.commentable.class == CollectionItem } }
+  expose (:recently_created_page_comments)           { Comment.where(approved: true).order('created_at DESC').select{|c| c.commentable.class == Page } }
+  expose (:recently_created_resource_comments)       { Comment.where(approved: true).order('created_at DESC').select{|c| c.commentable.class == Resource} }
 
   def index
   end
