c3036f85bd737fcec7c1392581002dc19754704c
mbassmvh <markvh@manchester.ac.uk>
2016-06-06 23:38:27 +0100

    refactored content html generator, improved help page content and styling

--- app/helpers/content_html_generator.rb
+++ app/helpers/content_html_generator.rb
@@ -11,45 +11,31 @@ class ContentHtmlGenerator
   @@close_double_square_invisible_pattern = /\)un78iJKlikely\)/
   @@open_double_square_visible  = '['
   @@close_double_square_visible = ']'
-  
-  
+
+ # -------------------------------------------------------------------------------------------------------
 
   def self.generate_full(page_or_collection_item)
-
-    new_markdown = ContentHtmlGenerator.chomp_this(page_or_collection_item)
-    new_markdown.gsub!(@@open_double_square_brackets_pattern) {@@open_double_square_invisible}
-    new_markdown.gsub!(@@close_double_square_brackets_pattern){@@close_double_square_invisible}
-
-    new_markdown.gsub!(@@link){LinkInterpreter.new($1).process}
-
-    markdown_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(), extensions = {})
-    rendered = markdown_renderer.render(new_markdown)
-
-    rendered.gsub!(@@open_double_square_invisible_pattern) {@@open_double_square_visible}
-    rendered.gsub!(@@close_double_square_invisible_pattern){@@close_double_square_visible}
-    rendered.html_safe
+    preprocess_and_make_markdown(page_or_collection_item){ |md| md.gsub!(@@link){LinkInterpreter.new($1).process} }
   end
 
   def self.generate_with_small_images(page_or_collection_item)
-    markdown_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(render_options = {}),
-                                                extensions = {})
-    new_markdown = ContentHtmlGenerator.chomp_this(page_or_collection_item).gsub(/\[([^\]]*)\]/) do
-                                          li = LinkInterpreter.new($1)
-                                          if li.image_url?
-                                            li.process_summary_image
-                                          elsif li.is_youtube_url? || li.is_vimeo_url?
-                                            li.process_summary_video
-                                          else
-                                            li.process
-                                          end
-                                        end
-
-    markdown_renderer.render(new_markdown).html_safe
+    preprocess_and_make_markdown(page_or_collection_item) do |md|
+      md.gsub!(@@link) do
+        li = LinkInterpreter.new($1)
+        if li.image_url?
+          li.process_summary_image
+        elsif li.is_youtube_url? || li.is_vimeo_url?
+          li.process_summary_video
+        else
+          li.process
+        end
+      end
+    end
   end
 
   def self.page_image(page_or_collection_item)
     image = nil
-    ContentHtmlGenerator.chomp_this(page_or_collection_item).gsub(/\[([^\]]*)\]/) do
+    (page_or_collection_item.description).gsub(/\[([^\]]*)\]/) do
       li = LinkInterpreter.new($1)
       image = li.url if ! image && li.image_url?
     end
@@ -57,12 +43,26 @@ class ContentHtmlGenerator
     image
   end
 
-  def self.chomp_this(page_or_collection_item)
-    if page_or_collection_item.respond_to?(:content)
-      page_or_collection_item.content
-    else
-      page_or_collection_item.description
-    end
+  def self.preprocess_and_make_markdown(page_or_collection_item)
+    markdown = preprocess_double_square_brackets( page_or_collection_item.description )
+    yield markdown
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(), extensions = {})
+    rendered = renderer.render(markdown)
+    post_process_double_square_brackets(rendered).html_safe
   end
+
+  def self.preprocess_double_square_brackets(markdown)
+    markdown.gsub!(@@open_double_square_brackets_pattern) {@@open_double_square_invisible}
+    markdown.gsub!(@@close_double_square_brackets_pattern){@@close_double_square_invisible}
+    markdown
+  end
+
+  def self.post_process_double_square_brackets(rendered)
+    rendered.gsub!(@@open_double_square_invisible_pattern) {@@open_double_square_visible}
+    rendered.gsub!(@@close_double_square_invisible_pattern){@@close_double_square_visible}
+    rendered
+  end
+
+
 
 end
--- config/initial_pages/help.md
+++ config/initial_pages/help.md
@@ -1,43 +1,67 @@
-To do anything else other than read this wiki, you need to register and log in.
+##Introduction
 
-For the moment, your registration needs to be approved. As we improve spam detection, this
-constraint will probably fall away. But please watch your email after you register.
+This special purpose wiki provides a virtual museum with collection items and additional information. The ocntent of the museum is intended to be crowdsourced. The wiki provides various facilities to aid in that process.
 
-This wiki is for cataloging collection items and adding background information. Thus 
-there a two page types
+You can read this wiki, navigating through it, without signing in.
 
-1. Collection Item pages
-2. Normal pages, without collection item information
+To contribute to the content and comment on content, you need to sign in. If you don't have an account yet you need to sign up for one, confirm your e-mail address via an e-mail that we send you, and then sign in. It's an easy process.
+
+For the moment, your sign up registration needs to be approved by an administrator. As we improve spam detection, the need for this will probably disappear. 
+
+##Collection items
+ 
+Collection Item pages, these are distinguished by having a item number (item in the collection) and a location (which assumes items in the collection are physical objects, stored or displayed somewhere). Ignore the location if that is nor relevant.
+
+## More information
+
+There may be pages of information that are not specifically about collection items. These pages may provide background information about the collection and collection items. E.g. biographical details about someone pertinent to the collection.
+
+## Using page and collection item titles as hyperlinks
+
+Within page and collection item content, you may create hyperlinks to pages and hyperlinks. When a user clicks on a hyperlink s/he is taken to the referenced page.
 
 Across both kinds of pages, page names need to be unique. This is needed for the wiki so
-you can embed references to other pages (or pages to be created) in square brackets \\[thus\\].
-Please be careful not to change page names.
+you can embed references to other pages (or pages to be created) in square brackets 
 
+- \[A page or collection item title\]
 
-Page content is also written using non-Markdown directives inside square brackets.
+## Hyperlinks
+
+When you are editing a page, please be aware that if you change the page name, you will break any  links to the page that appear on other pages.
+
+Page content may augmented by making other uses of square brackets
+written using non-Markdown directives inside square brackets.
 
 Page content is written in a formatting language called Markdown. 
 
 ## Using square brackets
 
 Put links to wiki pages in text:
+
 - \[existing page name\] to create a link to another page.
 - \[new page name\] for a new page.
 
 Put links to other pages on the web in text:
+
 - \[url optional-display-text\] to to create a link to create a new page.
 
 For images, YouTube videos, or Vimeo videos the following should be on lines by themselves
+
 - \[url-for-image optional-width\]
 - \[url-for-youtube-video optional-width\]
 - \[url-for-vimeo-video optional-width\]
 
 Titles, for successive levels start the line with # ## or ###
 
-List item starts with * with blank lines above and below list
+List item starts with * or - with blank lines above and below the list.
 
-Quote starts with >
-- replicate for successive quoted paragraphs
+A quote starts with > with blank lines above and below list thus
 
-Google markdown for more formatting or see the
-\[href://http://daringfireball.net/projects/markdown/syntax' Markdown Documentation\]
+> Quote text
+
+Replicate this for successive quoted paragraphs.
+
+##More about markdown
+
+Google markdown for more formatting help or see the
+[href://http://daringfireball.net/projects/markdown/syntax Markdown Documentation]
