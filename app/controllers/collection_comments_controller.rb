123f1e553c30747cd4675358fa358a23472c5c26
mbassmvh <markvh@manchester.ac.uk>
2016-06-08 13:35:59 +0100

    tidied comments routing for ci, p and r deleted unneeded collection comments controller

--- app/controllers/collection_comments_controller.rb
+++
@@ -1,6 +0,0 @@
-class CollectionCommentsController < CommentsController
-  expose(:collection_item, finder_parameter: :collection_item_id, finder: :find_by_slug)
-
-  #todo work out if this is needed
-
-end
