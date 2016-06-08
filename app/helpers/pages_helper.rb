2c02ac9d4b0ac6f0f8979d858161250756c62967
mbassmvh <markvh@manchester.ac.uk>
2016-06-08 23:35:55 +0100

    auto creation of pages works when first user is logged in

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -4,10 +4,12 @@ module PagesHelper
     pg = Page.new
     pg.description = File.read(Rails.root.join("config/initial_pages/#{pg_str}.md"))
     pg.title = Title.create(title: "SPECIAL: #{pg_str.capitalize}")
+    pg.name = "SPECIAL: #{pg_str.capitalize}"
     pg.set_tags_from_string( "#{pg_str}" )
     pg.set_categories_from_string( '')
-    pg.logged_user_id = 1
-    pg.user_id = pg.logged_user_id
+    pg.logged_user_id = current_user.id
+    pg.user_id = current_user.id
+    pg.creator = current_user
     pg.save
     pg
   end
