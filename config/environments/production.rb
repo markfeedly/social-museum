daf107a2632649d5f15da5620c1212ae12a5bdce
mbassmvh <markvh@manchester.ac.uk>
2016-06-21 16:24:00 +0100

    action mailer  default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ VirtualMuseum::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: ENV['URL'] }
 end
