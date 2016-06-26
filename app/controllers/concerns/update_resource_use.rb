84ae8fada2f9808b2f1e36ffa2fe7021832f96a3
mbassmvh <markvh@manchester.ac.uk>
2016-06-26 16:13:55 +0100

    small refactor of menu processing code for update of a resource after autocomplete

--- app/controllers/concerns/update_resource_use.rb
+++ app/controllers/concerns/update_resource_use.rb
@@ -3,11 +3,11 @@ module UpdateResourceUse
 
   def make_menu_items
     list = Title.where(Title.arel_table[:title].matches("%#{params[:term]}%")).pluck(:title, :id)
-    list.reject!{ |title_str, id| t = Title.find_by_id(id); t.title == title_str && t.titleable_type == 'Resource' if t }
+    list.reject! { |title_str, id| t = Title.find_by_id(id); t.title == title_str && t.titleable_type == 'Resource' if t }
     list.collect do |i|
       title = i[0]
       ttl = Title.find(i[1].to_i)
-      res_class_abbr =  ttl.titleable_type == 'Page' ? 'p' : 'c'
+      res_class_abbr = ttl.titleable_type == 'Page' ? 'p' : 'c'
       res_id = ttl.titleable_id.to_s
       puts '=== ' + title + '( ' + res_class_abbr + res_id + ')'
       title + ' {' + res_class_abbr + res_id + '}'
@@ -15,49 +15,46 @@ module UpdateResourceUse
   end
 
   def update_resource_use
-    begin
-      res_use_attrs = params[:resource][:resource_usages_attributes]
+    params[:resource][:resource_usages_attributes].collect { |key, value| value }.each do |selection|
+      case [selection['_destroy'] == '1' ? 'destroy' : 'no destroy',
+            selection['id'] == nil ? 'add' : 'allow destroy']
 
-      res_use_attrs.collect { |k, v| v }.each do |selection|
-        puts "==== sel #{selection}"
-        case_control = [selection['_destroy'] == '1' ? 'destroy' : 'no destroy', selection['id'] == nil ? 'add' : 'allow destroy']
-        puts "======= #{case_control}"
-        case case_control
+        when ['destroy', 'allow destroy']
+          typ, r_id = extract_class_and_id(selection)
+          res_use = ResourceUsage.find_by(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ)
+          res_use.delete if res_use
 
-          when ['destroy', 'allow destroy']
-            puts "*** in [destroy, allow destroy]"
-            typ = selection['page_title'] =~ / .c\d+.$/ ? 'CollectionItem' : 'Page'
-            r_id = selection['page_title'].match(/ {[pc](\d+)}$/)[1].to_i
-            destroy_list = ResourceUsage.where(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ)
-            puts "*--- destroy list #{destroy_list}"
-            destroy_list.first.delete unless destroy_list.empty?
-          when ['destroy', 'add']
-            puts "*** in [destroy, add] === do nothing"
-          when ['no destroy', 'add']
-            puts "*** in [no destroy, add]"
-            if selection['page_title'] != ''
-              typ = selection['page_title'] =~ / .c\d+.$/ ? 'CollectionItem' : 'Page'
-              r_id = selection['page_title'].match(/ {[pc](\d+)}$/)[1].to_i
-              puts "== typ #{typ}"
-              puts "== rid #{r_id}"
-              resourceable = (typ == 'CollectionItem' ? CollectionItem.find(r_id) : Page.find(r_id))
-              puts "== rid #{resourceable}"
-              unless ResourceUsage.where(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ)
-                ResourceUsage.create(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ)
-              end
+        when ['destroy', 'add']
+
+        when ['no destroy', 'add']
+          if selection['page_title'] != ''
+            typ, r_id = extract_class_and_id(selection)
+
+            puts "== selection #{selection}"
+            puts "== typ #{typ}"
+            puts "== rid #{r_id}"
+            resourceable = (typ == 'CollectionItem' ? CollectionItem.find(r_id) : Page.find(r_id))
+            puts "== rid #{resourceable}"
+            if ResourceUsage.where(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ).empty?
+              ResourceUsage.create(resource_id: resource.id, resourceable_id: r_id, resourceable_type: typ)
             end
+          end
+          ResourceUsage.where(resource_id: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resource_id: nil).blank?
+          ResourceUsage.where(resourceable_id: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resourceable_id: nil).blank?
+          ResourceUsage.where(resourceable_type: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resourceable_type: nil).blank?
 
-            ResourceUsage.where(resource_id: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resource_id: nil).blank?
-            ResourceUsage.where(resourceable_id: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resourceable_id: nil).blank?
-            ResourceUsage.where(resourceable_type: nil).each { |ru| ru.destroy } unless ResourceUsage.where(resourceable_type: nil).blank?
+        when ['no destroy', 'allow destroy']
 
-
-          when ['no destroy', 'allow destroy']
-            puts "*** in [no destroy, allow destroy] === do nothing"
-
-        end
       end
     end
+  end
+
+  private
+
+  def extract_class_and_id(selection)
+    typ = selection['page_title'] =~ / .c\d+.$/ ? 'CollectionItem' : 'Page'
+    r_id = selection['page_title'].match(/ {[pc](\d+)}$/)[1].to_i
+    [typ, r_id]
   end
 
 end
