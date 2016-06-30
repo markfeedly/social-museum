38738f78ac67c847ac0639b196d7eca88084ede2
mbassmvh <markvh@manchester.ac.uk>
2016-06-30 19:26:35 +0100

    oretty up format of categories controller

--- app/controllers/recent_controller.rb
+++ app/controllers/recent_controller.rb
@@ -1,12 +1,12 @@
 class RecentController < ApplicationController
-  expose (:recently_created_collection_items){CollectionItem.order('created_at DESC').limit(40)}
-  expose (:recently_created_pages){Page.order('created_at DESC').limit(40)}
-  expose (:recently_created_resources){Resource.order('created_at DESC').limit(40)}
+  expose (:recently_created_collection_items) { CollectionItem.order('created_at DESC').limit(40) }
+  expose (:recently_created_pages)            { Page.order('created_at DESC').limit(40) }
+  expose (:recently_created_resources)        { Resource.order('created_at DESC').limit(40) }
 
 
-  expose (:recently_edited_collection_items){CollectionItem.order('updated_at DESC').limit(40).select{|ci|ci.created_at!=ci.updated_at}}
-  expose (:recently_edited_pages){Page.order('updated_at DESC').limit(200).select{|p|p.created_at!=p.updated_at}[0..39]}
-  expose (:recently_edited_resources){Resource.order('updated_at DESC').limit(500).select{|p|p.created_at!=p.updated_at}[0..39]}
+  expose (:recently_edited_collection_items) { CollectionItem.order('updated_at DESC').limit(40).select{|ci|ci.created_at!=ci.updated_at}[0..39] }
+  expose (:recently_edited_pages)            { Page.order('updated_at DESC').limit(200).select{|p|p.created_at!=p.updated_at}[0..39] }
+  expose (:recently_edited_resources)        { Resource.order('updated_at DESC').limit(500).select{|p|p.created_at!=p.updated_at}[0..39] }
 
   expose (:recently_created_comments){Comment.where(approved: true).order('created_at DESC').limit(200)}
 
