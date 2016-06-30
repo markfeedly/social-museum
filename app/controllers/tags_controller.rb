7431e6a5f9fa8ae464e9990f911124616be194ae
mbassmvh <markvh@manchester.ac.uk>
2016-06-30 19:22:39 +0100

    bit of renaming in tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,25 +1,22 @@
 class TagsController < ApplicationController
 
-  expose(:tag_is) { Tag.where(name: params[:id]).first }
-  expose(:none)     { tag_is ? false : true  }
+  expose(:tag_from_param) { Tag.where(name: params[:id]).first }
+  expose(:none)    { tag_from_param ? false : true  }
 
-  expose(:tagged_collection_items) { tag_is ? tag_is.collection_items : [] }
-  expose(:tagged_pages) { tag_is ? tag_is.pages : [] }
-  expose(:tagged_resources) { tag_is ? tag_is.resources : [] }
+  expose(:tagged_collection_items) { none ? [] : tag_from_param.collection_items }
+  expose(:tagged_pages)            { none ? [] : tag_from_param.pages }
+  expose(:tagged_resources)        { none ? [] : tag_from_param.resources }
 
   expose(:paginated_tagged_collection_items) { Kaminari.paginate_array(tagged_collection_items).page(params[:page]).per(10) }
   expose(:paginated_tagged_pages) { Kaminari.paginate_array(tagged_pages).page(params[:page]).per(10) }
   expose(:paginated_tagged_resources) { Kaminari.paginate_array(tagged_resources).page(params[:page]).per(10) }
 
-  expose(:ci_count) { tag_is ? tagged_collection_items.count : 0 }
-  expose(:p_count)  { tag_is ? tagged_pages.count : 0 }
-  expose(:r_count)  { tag_is ? tagged_resources.count : 0 }
+  expose(:ci_count) { none ? 0 : tagged_collection_items.count }
+  expose(:p_count)  { none ? 0 : tagged_pages.count }
+  expose(:r_count)  { none ? 0 : tagged_resources.count }
 
   expose(:ci_class) { ci_count > 0 ? 'active' : '' }
   expose(:p_class)  { ci_count == 0 && p_count > 0 ? 'active' : '' }
   expose(:r_class)  { ci_count == 0 && p_count == 0 && r_count > 0 ? 'active' : '' }
-
-  def show
-  end
 
 end
