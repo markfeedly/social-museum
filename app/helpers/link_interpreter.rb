0546a1cd35dd67f4b3646941b37c84f299620080
mbassmvh <markvh@manchester.ac.uk>
2016-07-04 13:42:34 +0100

    mid trying to refactor link interpreter

--- app/helpers/link_interpreter.rb
+++ app/helpers/link_interpreter.rb
@@ -2,7 +2,6 @@ require 'uri'
 require 'pp'
 
 class LinkInterpreter
-  #TODO Large restructure of this file
   def url
     @first
   end
@@ -10,6 +9,126 @@ class LinkInterpreter
   def initialize text
     @text = text.strip
     @first, @rest = @text.split(/ +/, 2)
+    @width = @rest.split(/ +/).to_i
+    @width = false if @width <= 0
+  end
+
+  def process
+    if is_rel_url?
+      process_rel_url
+    elsif page_title?
+      process_title
+    elsif image_url?
+      process_image_url
+    elsif is_youtube_url?
+      process_youtube_url
+    elsif is_vimeo_url?
+      process_vimeo_url
+    else
+      process_url
+    end
+  end
+
+  #-----
+
+  #TODO refactor
+  def process_rel_url
+    @first.gsub!(/rel:\/\//, ENV['SITE']+'/')
+    process_url
+  end
+
+  def is_rel_url?
+    !! (@first =~ /^rel:\/\//)
+  end
+
+  #-----
+
+  def page_title?
+    ! (url? || empty?)
+  end
+
+  def process_title
+    title = Title.where(title: @text).first
+    if title
+      case title.titleable
+        when Page
+          "<a href='/pages/#{title.slug}' data-page>#{@text}</a>"
+        when CollectionItem
+          "<a href='/collection_items/#{title.slug}' data-collection-item>#{@text}</a>"
+        when Resource
+          "<a href='/resources/#{title.slug}' data-resource>#{@text}</a>"
+      end
+    else
+      if /^_/ =~ @text
+        "<a href='/resources/new?resource_title=#{@text}' data-new-resource>#{@text}</a>"
+      else
+        "<a href='/pages/new?page_title=#{@text}' data-new-page>#{@text}</a>"
+      end
+    end
+  end
+
+#-----
+
+  def image_url?
+    url_suffix? && ( !!(@first.downcase =~ /\.png$/) || !!(@first.downcase =~ /\.jpg$/) || !!(@first.downcase =~ /\.jpeg$/) || !!(@first.downcase =~ /\.gif/))
+  end
+
+  def process_image_url
+    if @width then
+      "<img src='#{@first}' style='width: #{@width}px;'/>"
+    else
+      "<img src='#{@text}'/>"
+    end
+  end
+
+#-----
+
+  def is_youtube_url?
+    is_domain? 'youtube.com'
+  end
+
+  def process_youtube_url
+    uri    = URI.parse(@first)
+    params = Hash[uri.query.split('&').map{|p| p.split('=')}] if uri.query
+
+    if params
+      process_video_url("youtube.com/embed/#{params['v']}")
+    else
+      process_url
+    end
+  end
+
+#-----
+
+  def is_vimeo_url?
+    is_domain? 'vimeo.com'
+  end
+
+  def process_vimeo_url
+    uri = URI.parse(@first)
+    id = uri.path.split('/').last
+
+    if id
+      process_video_url("player.vimeo.com/video/#{id}")
+    else
+      process_url
+    end
+  end
+
+  #-----
+
+  def process_url
+    if @width then
+      "<a href='#{@first}' external-link>#{@rest}</a>"
+    else
+      "<a href='#{@text}' external-link>#{@text}</a>"
+    end
+  end
+
+#-------------------
+
+  def url_suffix?
+    url? && ( !!(@first =~ /:\/\/..*\/..*/   ))
   end
 
   def url?
@@ -19,9 +138,6 @@ class LinkInterpreter
     false
   end
 
-  def page_title?
-    ! (url? || empty?)
-  end
 
   def empty?
     @text.blank?
@@ -35,13 +151,8 @@ class LinkInterpreter
     !(@uri.host.nil? || @uri.host.include?(".."))
   end
 
-  def url_suffix?
-    url? && ( !!(@first =~ /:\/\/..*\/..*/   ))
-  end
 
-  def image_url?
-    url_suffix? && ( !!(@first.downcase =~ /\.png$/) || !!(@first.downcase =~ /\.jpg$/) || !!(@first.downcase =~ /\.jpeg$/) || !!(@first.downcase =~ /\.gif/))
-  end
+
 
   def domain
     return nil unless url?
@@ -56,33 +167,9 @@ class LinkInterpreter
     !!(domain.match(domain_to_match))
   end
 
-  def is_youtube_url?
-    is_domain? 'youtube.com'
-  end
 
-  def is_vimeo_url?
-    is_domain? 'vimeo.com'
-  end
 
-  def process_title
-    title = Title.where(title: @text).first
-    if title
-      case title.titleable
-      when Page
-        "<a href='/pages/#{title.slug}' data-page>#{@text}</a>"
-      when CollectionItem
-        "<a href='/collection_items/#{title.slug}' data-collection-item>#{@text}</a>"
-        when Resource
-          "<a href='/resources/#{title.slug}' data-resource>#{@text}</a>"
-      end
-    else
-      if /^_/ =~ @text
-        "<a href='/resources/new?resource_title=#{@text}' data-new-resource>#{@text}</a>"
-      else
-        "<a href='/pages/new?page_title=#{@text}' data-new-page>#{@text}</a>"
-      end
-    end
-  end
+
 
   def process_url_without_text
     "<a href='#{@text}' external-link>#{@text}</a>"
@@ -92,61 +179,22 @@ class LinkInterpreter
     "<a href='#{@first}' external-link>#{@rest}</a>"
   end
 
-  #TODO refactor
-  def process_rel_url
-    @first.gsub!(/rel:\/\//, ENV['SITE']+'/')
-    process_url
-  end
 
-  def is_rel_url?
-    !! (@first =~ /^rel:\/\//)
-  end
 
-  def process_url
-    @rest ? process_url_with_text : process_url_without_text
-  end
 
-  def process_image_url_without_width
-    "<img src='#{@text}'/>"
-  end
 
-  def process_image_url_with_width
-    "<img src='#{@first}' style='width: #{@rest}px;'/>"
-  end
+
+
 
   def process_resource_image
-    "<ing src=#{@rest.split(' ')[0]}' style='width: #{rest.split(' ')[1]}px;'/>"
+    "<ing src=#{@rest.split(/ +/)[0]}' style='width: #{rest.split(/ +/)[1]}px;'/>"
   end
 
-  def process_reosurce_image_with_width
+  def process_resource_image_with_width
     "<ing src=#{rest.split(' ')[0]}' />"
   end
 
-  def process_image_url
-    @rest ? process_image_url_with_width : process_image_url_without_width
-  end
 
-  def process_youtube_url
-    uri    = URI.parse(@first)
-    params = Hash[uri.query.split('&').map{|p| p.split('=')}] if uri.query
-
-    if params
-      process_video_url("youtube.com/embed/#{params['v']}")
-    else
-      process_url
-    end
-  end
-
-  def process_vimeo_url
-    uri = URI.parse(@first)
-    id = uri.path.split('/').last
-
-    if id
-      process_video_url("player.vimeo.com/video/#{id}")
-    else
-      process_url
-    end
-  end
 
   def process_video_url(video_url)
     @rest &&= @rest.split(' ', 2)[0]
@@ -161,27 +209,6 @@ class LinkInterpreter
       :in_div__hyperlink
     else
       :in_line_hyperlink
-    end
-  end
-
-  def process
-    if is_rel_url?
-      process_rel_url
-
-    elsif page_title?
-      process_title
-
-    elsif image_url?
-      process_image_url
-
-    elsif is_youtube_url?
-      process_youtube_url
-
-    elsif is_vimeo_url?
-      process_vimeo_url
-
-    else
-      process_url
     end
   end
 
