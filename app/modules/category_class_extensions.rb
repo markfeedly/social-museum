1ca5fbe1ae21dc54ebd8f3e68f8001e4fa750c28
mbassmvh <markvh@manchester.ac.uk>
2016-07-08 20:56:44 +0100

    get_children & tests

--- app/modules/category_class_extensions.rb
+++ app/modules/category_class_extensions.rb
@@ -1,13 +1,21 @@
 module CategoryClassExtensions
 
-  def get_categories
-    f = Rails.root.join(ENV['CATEGORIES_FILE_RELATIVE_PATH'])
-    cats = YAML.load( File.open(f) )
-    cats[0].to_a[0][1].map{|c| c.to_a[0]}.map{|c| [c[0], :isa, c[1]]}
+  def get_children(predicate, root)
+    matched_triples = Category.get_matches(nil, predicate, root)
+    return [root] unless matched_triples
+    matches = matched_triples.collect{ |t| t[0] }
+    non_unique = matches.collect{|child| get_children(predicate, child)}.flatten
+    Set.new(non_unique+[root]).to_a.sort
   end
 
   def get_matches(subject=nil, predicate=nil, object=nil)
     get_categories.select{ |t| subject ? subject==t[0] : true }.select{ |t| predicate ? predicate==t[1] : true }.select{ |t| object ? object==t[2] : true }
+  end
+
+  def get_categories
+    f = Rails.root.join(ENV['CATEGORIES_FILE_RELATIVE_PATH'])
+    cats = YAML.load( File.open(f) )
+    cats[0].to_a[0][1].map{|c| c.to_a[0]}.map{|c| [c[0], :isa, c[1]]}
   end
 
 end
--- spec/models/category_class_extensions_spec.rb
+++ spec/models/category_class_extensions_spec.rb
@@ -8,15 +8,15 @@ describe 'CollectionItem Categories' do
 
   let(:user) { FactoryGirl.create(:user) }
   let(:collection_item) { new_collection_item }
-  let(:all_triples) { [["Manchester computer", :isa, "Computer"],
-                       ["Ferranti Mark I", :isa, "Manchester computer"],
-                       ["MU5", :isa, "Manchester computer"],
-                       ["MU6G", :isa, "Manchester computer"],
-                       ["SSEM", :isa, "Manchester computer"],
-                       ["Atlas", :isa, "Manchester computer"],
-                       ["VUM Atlas", :isa, "Atlas"],
-                       ["Other computer", :isa, "Computer"],
-                       ["PDP-11", :isa, "Other computer"]] }
+  let(:all_triples) { [['Manchester computer', :isa, 'Computer'],
+                       ['Ferranti Mark I', :isa, 'Manchester computer'],
+                       ['MU5', :isa, 'Manchester computer'],
+                       ['MU6G', :isa, 'Manchester computer'],
+                       ['SSEM', :isa, 'Manchester computer'],
+                       ['Atlas', :isa, 'Manchester computer'],
+                       ['VUM Atlas', :isa, 'Atlas'],
+                       ['Other computer', :isa, 'Computer'],
+                       ['PDP-11', :isa, 'Other computer']] }
 
   it "should get categories" do
     expect(collection_item.get_categories).to eq all_triples
@@ -25,20 +25,24 @@ describe 'CollectionItem Categories' do
 
   end
 
-  it "should get a fully specified triple" do
-    expect(Category.get_matches('MU5', :isa, 'Computer')).to eq [["MU5", :isa, "Manchester computer"]]
+  it "should match triples" do
+    expect(Category.get_matches(nil,  nil,  nil) ).to eq all_triples
+    expect(Category.get_matches(nil,  nil,  'Manchester computer')).to eq [['Ferranti Mark I', :isa, 'Manchester computer'], ['MU5', :isa, 'Manchester computer'], ['MU6G', :isa, 'Manchester computer'], ['SSEM', :isa, 'Manchester computer'], ['Atlas', :isa, 'Manchester computer']]
+    expect(Category.get_matches(nil,  :isa, nil)).to eq all_triples
+    expect(Category.get_matches(nil,  :isa, 'Manchester computer')).to eq [['Ferranti Mark I', :isa, 'Manchester computer'], ['MU5', :isa, 'Manchester computer'], ['MU6G', :isa, 'Manchester computer'], ['SSEM', :isa, 'Manchester computer'], ['Atlas', :isa, 'Manchester computer']]
+
+    expect(Category.get_matches('MU5', nil, nil)).to eq [['MU5', :isa, 'Manchester computer']]
+    expect(Category.get_matches('MU5', nil, 'Manchester computer')).to eq [['MU5', :isa, 'Manchester computer']]
+    expect(Category.get_matches('MU5', :isa, nil)).to eq [['MU5', :isa, 'Manchester computer']]
+    expect(Category.get_matches('MU5', :isa, 'Manchester computer')).to eq [['MU5', :isa, 'Manchester computer']]
   end
 
-  it "should get a single triple" do
-    expect(Category.get_matches(nil,  nil,  nil) ).to eq all_triples
-    expect(Category.get_matches(nil,  nil,  "Manchester computer")).to eq [["Ferranti Mark I", :isa, "Manchester computer"], ["MU5", :isa, "Manchester computer"], ["MU6G", :isa, "Manchester computer"], ["SSEM", :isa, "Manchester computer"], ["Atlas", :isa, "Manchester computer"]]
-    expect(Category.get_matches(nil,  :isa, nil)).to eq all_triples
-    expect(Category.get_matches(nil,  :isa, "Manchester computer")).to eq [["Ferranti Mark I", :isa, "Manchester computer"], ["MU5", :isa, "Manchester computer"], ["MU6G", :isa, "Manchester computer"], ["SSEM", :isa, "Manchester computer"], ["Atlas", :isa, "Manchester computer"]]
+  it "should find children" do
+    expect(Category.get_children(:isa, 'Other computer')).to eq ['Other computer', 'PDP-11']
+    expect(Category.get_children(:isa, 'Atlas')).to eq ['Atlas', 'VUM Atlas']
+    expect(Category.get_children(:isa, 'VUM Atlas')).to eq ['VUM Atlas']
+    expect(Category.get_children(:isa, 'Manchester computer')).to eq ['Atlas', 'Ferranti Mark I', 'MU5', 'MU6G', 'Manchester computer', 'SSEM', 'VUM Atlas']
 
-    expect(Category.get_matches('MU5', nil, nil)).to eq [["MU5", :isa, "Manchester computer"]]
-    expect(Category.get_matches('MU5', nil, "Manchester computer")).to eq [["MU5", :isa, "Manchester computer"]]
-    expect(Category.get_matches('MU5', :isa, nil)).to eq [["MU5", :isa, "Manchester computer"]]
-    expect(Category.get_matches('MU5', :isa, "Manchester computer")).to eq [["MU5", :isa, "Manchester computer"]]
   end
 
 end
