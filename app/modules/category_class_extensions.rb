c04be9355474c3eeb5fd28b9e1c458712cf25339
mbassmvh <markvh@manchester.ac.uk>
2016-07-09 13:05:51 +0100

    minor refactor of category class extensions remove commented code

--- app/modules/category_class_extensions.rb
+++ app/modules/category_class_extensions.rb
@@ -25,12 +25,7 @@ module CategoryClassExtensions
   def get_categories
     f = Rails.root.join(ENV['CATEGORIES_FILE_RELATIVE_PATH'])
     cats = YAML.load( File.open(f) )
-    triples = cats[0].to_a[0][1].map{|c| c.to_a[0]}.map{|c| [c[0], :isa, c[1]]}
-=begin
-    names = triples.collect{|t| t[0]}
-    names.each{|n| Category.new(name: n) if Category.where(name: n).count == 0 }
-=end
-    triples
+    cats[0].to_a[0][1].map{|c| c.to_a[0]}.map{|c| [c[0], :isa, c[1]]}
   end
 
 end
