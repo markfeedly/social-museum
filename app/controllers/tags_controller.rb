d06764e093ec8960219ab1ff92d9c4078e32ff25
mbassmvh <markvh@manchester.ac.uk>
2017-03-05 23:18:09 +0000

    finished category and tab controller and views and partials refactor of  names exposed in the controllers -    much cleaner, resources and tab ordering not checked but look ok

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,17 +2,17 @@ class TagsController < ApplicationController
 
   expose(:tag_from_param) { Tag.where(name: params[:id]).first }
 
-  expose(:tagged_collection_items) { none ? [] : tag_from_param.collection_items }
-  expose(:tagged_pages)            { none ? [] : tag_from_param.pages }
-  expose(:tagged_resources)        { none ? [] : tag_from_param.resources }
+  expose(:tagged_collection_items) { tag_from_param.collection_items }
+  expose(:tagged_pages)            { tag_from_param.pages }
+  expose(:tagged_resources)        { tag_from_param.resources }
 
   expose(:paginated_collection_items) { Kaminari.paginate_array(tagged_collection_items).page(params[:page]).per(10) }
-  expose(:paginated_pages) { Kaminari.paginate_array(tagged_pages).page(params[:page]).per(10) }
-  expose(:paginated_resources) { Kaminari.paginate_array(tagged_resources).page(params[:page]).per(10) }
+  expose(:paginated_pages)            { Kaminari.paginate_array(tagged_pages).page(params[:page]).per(10) }
+  expose(:paginated_resources)        { Kaminari.paginate_array(tagged_resources).page(params[:page]).per(10) }
 
-  expose(:ci_count) { none ? 0 : tagged_collection_items.count }
-  expose(:p_count)  { none ? 0 : tagged_pages.count }
-  expose(:r_count)  { none ? 0 : tagged_resources.count }
+  expose(:ci_count) { tagged_collection_items.count }
+  expose(:p_count)  { tagged_pages.count }
+  expose(:r_count)  { tagged_resources.count }
 
   expose(:none)     { ci_count + p_count + r_count == 0 }
 
