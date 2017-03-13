3c201085b66a42eea53edc17e48957d5573fc2f9
mbassmvh <markvh@manchester.ac.uk>
2017-03-13 16:24:06 +0000

    page conflict view refactored

--- app/controllers/concerns/check_conflicting_changes.rb
+++ app/controllers/concerns/check_conflicting_changes.rb
@@ -1,20 +1,4 @@
 module CheckConflictingChanges
-=begin
-  def changed_collection_item?(coll_item)
-
-    desired_tags        = clean_str( params[:collection_item][:tags_as_str] )
-    desired_categories  = clean_str( params[:collection_item][:categories_as_str] )
-    desired_location    = params[:collection_item][:location]
-    desired_item_number = params[:collection_item][:item_number]
-    desired_description = params[:collection_item][:description]
-
-    coll_item.tags_as_str != desired_tags ||
-        coll_item.categories_as_str != desired_categories ||
-        coll_item.location != desired_location ||
-        coll_item.item_number != desired_item_number ||
-        coll_item.description != desired_description
-  end
-=end
 
   def changed_object?( obj, param_key )
     change = false
@@ -26,6 +10,8 @@ module CheckConflictingChanges
     end
     change || changed_common_part?(obj, param_key)
   end
+
+  private
 
   def changed_common_part?( obj, param_key )
     desired_tags        = clean_str( params[param_key][:tags_as_str] )
@@ -45,8 +31,6 @@ module CheckConflictingChanges
   def changed_resource_specific_part?(res)
     raise "in changed_resource_specific_part?"
   end
-
-  private
 
   def clean_str(str)
     str.split(',').collect{|t| t.strip.squeeze(' ')}.sort.uniq.reject{|t|t==''}.join(', ')
